1419d745dc97200c9117517c1d32e81536bf9d9d
Gokhan Sihli <gokhansihli@gmail.com>
2026-04-16 21:06:38 +0100

    Created structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { useState } from "react";
+import SearchBar from "./components/SearchBar";
 
 function App() {
-  return;
+  const [word, setWord] = useState("");
+  const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  return (
+    <>
+      <SearchBar word={word} setWord={setWord} />
+    </>
+  );
 }
 
 export default App;
