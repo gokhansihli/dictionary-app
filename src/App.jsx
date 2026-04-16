3c5497db33723e638e366709fed6ec9daa408ea8
Gokhan Sihli <gokhansihli@gmail.com>
2026-04-16 21:40:53 +0100

    Added handleSearch

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 import "./App.css";
 import { useState } from "react";
 import SearchBar from "./components/SearchBar";
+import { fetchWord } from "./api";
 
 function App() {
   const [word, setWord] = useState("");
   const [result, setResult] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+
+  const handleSearch = async () => {
+    if (!word) return;
+
+    setLoading(true);
+    setError("");
+    setResult(null);
+
+    try {
+      const data = await fetchWord(word);
+      setResult(data);
+    } catch (error) {
+      setError(error.message);
+    }
+
+    setLoading(false);
+  };
+
   return (
     <>
-      <SearchBar word={word} setWord={setWord} />
+      <SearchBar word={word} setWord={setWord} onSearch={handleSearch} />
     </>
   );
 }
