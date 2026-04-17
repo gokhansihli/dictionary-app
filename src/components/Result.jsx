627f11450665bca6bbd2f7ee944d7803e708e592
Gokhan Sihli <gokhansihli@gmail.com>
2026-04-17 23:05:59 +0100

    Implemented meanings and definitions

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,13 +1,23 @@
 export default function Result({ data }) {
+  console.log(data);
   return (
     <div>
-      {data.map((entry, i) => {
-        return (
-          <div key={i}>
-            <h2>{entry.word}</h2>
-          </div>
-        );
-      })}
+      {data.map((entry, i) => (
+        <div key={i}>
+          <h2>{entry.word}</h2>
+          {entry.meanings.map((meaning, j) => (
+            <div key={j}>
+              <h4>{meaning.partOfSpeech}</h4>
+              {meaning.definitions.map((def, k) => (
+                <div key={k}>
+                  <p>{def.definition}</p>
+                  {def.example && <em>Example: {def.example}</em>}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
